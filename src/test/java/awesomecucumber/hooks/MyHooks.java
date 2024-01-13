123054a9ced4fb69ab0b4f76a5452654584249d5
Mihir992 <119054373+Mihir992@users.noreply.github.com>
2024-01-13 18:20:09 +0530

    Update MyHooks-13-01-2024.java

--- src/test/java/awesomecucumber/hooks/MyHooks.java
+++ src/test/java/awesomecucumber/hooks/MyHooks.java
@@ -2,20 +2,31 @@ package awesomecucumber.hooks;
 
 import org.openqa.selenium.WebDriver;
 
+import awesomecucumber.context.TestContext;
 import awesomecucumber.factory.DriverFactory;
 import io.cucumber.java.After;
 import io.cucumber.java.Before;
+import io.cucumber.messages.types.Scenario;
 
 public class MyHooks {
 	private WebDriver driver;
+	private final TestContext context;
+	
+	public MyHooks(TestContext context) {
+		this.context = context;
+	}
+	
 	
 	@Before
-	public void before() {
-		driver = DriverFactory.initializeDriver();	
+	public void before(Scenario scenario) {
+		System.out.println("BEFORE: THREAD ID:" + Thread.currentThread().getId() + "," + "SCENARIO NAME: " + scenario.getName());
+		driver = DriverFactory.initializeDriver(System.getProperty("browser","chrome"));	
+		context.driver = driver;
 	}
 	
 	@After
-	public void after() {
+	public void after(Scenario scenario) {
+		System.out.println("AFTER: THREAD ID:" + Thread.currentThread().getId() + "," + "SCENARIO NAME: " + scenario.getName());
 		driver.quit();
 	}
 	
